d34b976e1c2e7becb9ec88677b56472c4dcc2c0b
ericliuhusky <289471838@qq.com>
2022-10-11 18:19:57 +0800

    ch1

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,38 @@
 #![no_std]
 #![no_main]
 
+use core::arch::global_asm;
+
 mod puts;
 mod exit;
 mod rust_no_std;
+mod print;
+
+global_asm!(include_str!("entry.s"));
 
 #[no_mangle]
-fn _start() {
-    puts::puts("Hello, world!\n");
-
+fn rust_main() {
+    extern "C" {
+        fn stext(); // begin addr of text segment
+        fn etext(); // end addr of text segment
+        fn srodata(); // start addr of Read-Only data segment
+        fn erodata(); // end addr of Read-Only data ssegment
+        fn sdata(); // start addr of data segment
+        fn edata(); // end addr of data segment
+        fn sbss(); // start addr of BSS segment
+        fn ebss(); // end addr of BSS segment
+        fn boot_stack(); // stack bottom
+        fn boot_stack_top(); // stack top
+    }
+    
+    println!(".text [{:#x}, {:#x})", stext as usize, etext as usize);
+    println!(".rodata [{:#x}, {:#x})", srodata as usize, erodata as usize);
+    println!(".data [{:#x}, {:#x})", sdata as usize, edata as usize);
+    println!(
+        "boot_stack [{:#x}, {:#x})",
+        boot_stack as usize, boot_stack_top as usize
+    );
+    println!(".bss [{:#x}, {:#x})", sbss as usize, ebss as usize);
+    
     exit::exit();
 }
