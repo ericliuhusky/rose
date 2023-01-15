c5652598a49cacf79f0c99bafdf04983d87b79e0
EricLiu <289471838@qq.com>
2023-01-15 11:03:30 +0800

    使用斐波那契函数来模拟超过10ms的耗时程序

--- ch3/user/src/bin/00write_a.rs
+++ ch3/user/src/bin/00write_a.rs
@@ -4,10 +4,13 @@
 #[macro_use]
 extern crate user;
 
+use user::fibonacci;
+
 #[no_mangle]
 fn main() -> i32 {
-    for i in 1..=300 {
-        println!("A [{}/{}]", i, 300);
+    for i in 1..=30 {
+        let r = fibonacci(i);
+        println!("A [{}/{}] {}", i, 30, r);
     }
     0
 }
--- ch3/user/src/lib.rs
+++ ch3/user/src/lib.rs
@@ -17,3 +17,10 @@ fn _start() {
         exit::exit(main());
     }
 }
+
+// 一个耗时程序，用以验证10ms之后会自动切换下一个任务
+pub fn fibonacci(x: u32) -> u32 {
+    if x == 0 { return 0 }
+    if x == 1 { return 1 }
+    fibonacci(x - 2) + fibonacci(x - 1)
+}
