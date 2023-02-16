16bf0c22a72b287dc7fea2046ffbad65a3665c46
EricLiu <289471838@qq.com>
2023-02-16 15:06:08 +0800

    汉化

--- ch1/src/main.rs
+++ ch1/src/main.rs
@@ -2,37 +2,91 @@
 #![no_main]
 
 use core::arch::global_asm;
-
-mod puts;
-mod exit;
-mod rust_no_std;
-mod print;
+use 退出模块::退出;
 
 global_asm!(include_str!("entry.s"));
 
 #[no_mangle]
 fn rust_main() {
     extern "C" {
-        fn stext(); // begin addr of text segment
-        fn etext(); // end addr of text segment
-        fn srodata(); // start addr of Read-Only data segment
-        fn erodata(); // end addr of Read-Only data ssegment
-        fn sdata(); // start addr of data segment
-        fn edata(); // end addr of data segment
-        fn sbss(); // start addr of BSS segment
-        fn ebss(); // end addr of BSS segment
-        fn boot_stack(); // stack bottom
-        fn boot_stack_top(); // stack top
+        fn stext(); // text段起始地址
+        fn etext(); // text段结束地址
+        fn srodata(); // 只读数据段起始地址
+        fn erodata(); // 只读数据段结束地址
+        fn sdata(); // 数据段起始地址
+        fn edata(); // 数据段结束地址
+        fn sbss(); // bss段起始地址
+        fn ebss(); // bss段结束地址
+        fn boot_stack(); // 栈底
+        fn boot_stack_top(); // 栈顶
     }
     
-    println!(".text [{:#x}, {:#x})", stext as usize, etext as usize);
-    println!(".rodata [{:#x}, {:#x})", srodata as usize, erodata as usize);
-    println!(".data [{:#x}, {:#x})", sdata as usize, edata as usize);
-    println!(
+    格式化输出并换行!(".text [{:#x}, {:#x})", stext as usize, etext as usize);
+    格式化输出并换行!(".rodata [{:#x}, {:#x})", srodata as usize, erodata as usize);
+    格式化输出并换行!(".data [{:#x}, {:#x})", sdata as usize, edata as usize);
+    格式化输出并换行!(
         "boot_stack [{:#x}, {:#x})",
         boot_stack as usize, boot_stack_top as usize
     );
-    println!(".bss [{:#x}, {:#x})", sbss as usize, ebss as usize);
+    格式化输出并换行!(".bss [{:#x}, {:#x})", sbss as usize, ebss as usize);
     
-    exit::exit();
+    退出();
+}
+
+mod 输出模块 {
+    use core::arch::asm;
+    use core::fmt::{self, Write};
+
+    fn 输出(字符串: &str) {
+        for 字符 in 字符串.chars() {
+            unsafe {
+                asm!(
+                    "ecall",
+                    in("x10") 字符 as usize,
+                    in("x17") 1
+                );
+            }
+        }
+    }
+
+    struct 标准输出;
+    impl Write for 标准输出 {
+        fn write_str(&mut self, s: &str) -> fmt::Result {
+            输出(s);
+            Ok(())
+        }
+    }
+    pub fn 格式化输出(参数: fmt::Arguments) {
+        标准输出.write_fmt(参数).unwrap();
+    }
+
+    #[macro_export]
+    macro_rules! 格式化输出并换行 {
+        ($fmt: literal $(, $($arg: tt)+)?) => {
+            $crate::输出模块::格式化输出(format_args!(concat!($fmt, "\n") $(, $($arg)+)?));
+        }
+    }
+}
+
+mod 退出模块 {
+    use core::arch::asm;
+
+    pub fn 退出() {
+        unsafe {
+            asm!(
+                "sw {0}, 0({1})",
+                in(reg)0x5555, in(reg)0x100000
+            );
+        }
+    }
+}
+
+mod rust裸机无标准库 {
+    use core::panic::PanicInfo;
+
+    // 需要提供崩溃处理
+    #[panic_handler]
+    fn panic(_info: &PanicInfo) -> ! {
+        loop {}
+    }
 }
