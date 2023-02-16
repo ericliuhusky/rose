5ffdcd43dba10149e2e9609a9cdcba65a16ab650
EricLiu <289471838@qq.com>
2023-02-16 14:38:59 +0800

    汉化

--- helloworld/src/main.rs
+++ helloworld/src/main.rs
@@ -1,13 +1,50 @@
 #![no_std]
 #![no_main]
 
-mod puts;
-mod exit;
-mod rust_no_std;
+use 输出模块::输出;
+use 退出模块::退出;
 
 #[no_mangle]
 fn _start() {
-    puts::puts("Hello, world!\n");
+    输出("Hello, world!\n");
+    退出();
+}
 
-    exit::exit();
+mod 输出模块 {
+    use core::arch::asm;
+
+    pub fn 输出(字符串: &str) {
+        for 字符 in 字符串.chars() {
+            unsafe {
+                asm!(
+                    "ecall",
+                    in("x10") 字符 as usize,
+                    in("x17") 1
+                );
+            }
+        }
+    }
+}
+
+mod 退出模块 {
+    use core::arch::asm;
+
+    pub fn 退出() {
+        unsafe {
+            asm!(
+                "sw {0}, 0({1})",
+                in(reg)0x5555, in(reg)0x100000
+            );
+        }
+    }
+}
+
+mod rust裸机无标准库 {
+    use core::panic::PanicInfo;
+
+    // 需要提供崩溃处理
+    #[panic_handler]
+    fn panic(_info: &PanicInfo) -> ! {
+        loop {}
+    }
 }
