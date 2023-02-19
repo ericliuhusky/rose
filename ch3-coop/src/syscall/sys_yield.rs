51a68d302ae37792117e2bf23edde481c29a30ff
EricLiu <289471838@qq.com>
2023-02-19 11:27:46 +0800

    yield

--- ch3-coop/src/syscall/sys_yield.rs
+++
@@ -1,6 +0,0 @@
-use crate::task::suspend_current_and_run_next;
-
-pub fn sys_yield() -> isize {
-    suspend_current_and_run_next();
-    0
-}
--- ch3-coop/src/task/switch.rs
+++
@@ -1,8 +0,0 @@
-use super::context::TaskContext;
-use core::arch::global_asm;
-
-global_asm!(include_str!("switch.s"));
-
-extern "C" {
-    pub fn __switch(current_task_cx_ptr: *mut TaskContext, next_task_cx_ptr: *const TaskContext);
-}
