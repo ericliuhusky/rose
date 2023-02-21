afd7de46b975a6056870e5a5758f00414fa854f0
EricLiu <289471838@qq.com>
2023-02-21 12:38:03 +0800

    default_alloc_error_handler

--- ch4/src/main.rs
+++ ch4/src/main.rs
@@ -1,6 +1,6 @@
 #![no_std]
 #![no_main]
-#![feature(alloc_error_handler)]
+#![feature(default_alloc_error_handler)]
 
 extern crate alloc;
 
--- ch4/src/mm/heap_allocator.rs
+++ ch4/src/mm/heap_allocator.rs
@@ -5,11 +5,6 @@ static mut 内核堆: [u8; 0x30_0000] = [0; 0x30_0000];
 #[global_allocator]
 static 堆管理器: LockedHeap = LockedHeap::empty();
 
-#[alloc_error_handler]
-pub fn handle_alloc_error(layout: core::alloc::Layout) -> ! {
-    panic!("Heap allocation error, layout = {:?}", layout);
-}
-
 pub fn 初始化() {
     unsafe {
         堆管理器
