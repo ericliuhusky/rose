d368ca330ba0b2bd7b59d72a15cb8741aa46b047
EricLiu <289471838@qq.com>
2023-02-21 10:56:48 +0800

    KERNEL_STACK_SIZE

--- ch4/src/config.rs
+++ ch4/src/config.rs
@@ -1,12 +1,11 @@
 pub const USER_STACK_SIZE: usize = 4096 * 2;
-pub const KERNEL_STACK_SIZE: usize = 4096 * 2;
 pub const KERNEL_HEAP_SIZE: usize = 0x30_0000;
 pub const MEMORY_END: usize = 0x80800000;
 
 pub const TRAP_CONTEXT_END: usize = 0xfffffffffffff000;
 pub const TRAP_CONTEXT: usize = 0xffffffffffffe000;
 pub const 内核栈栈顶: usize = 0xfffffffffffff000;
-pub const 内核栈栈底: usize = 内核栈栈顶 - KERNEL_STACK_SIZE;
+pub const 内核栈栈底: usize = 0xffffffffffffd000;
 
 pub const MMIO: &[(usize, usize)] = &[
     (0x0010_0000, 0x00_2000), // VIRT_TEST/RTC  in virt machine
