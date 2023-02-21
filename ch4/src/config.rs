a2598c2deaed47078d7e13cc705fc1fbddd48186
EricLiu <289471838@qq.com>
2023-02-21 10:55:44 +0800

    PAGE_SIZE

--- ch4/src/config.rs
+++ ch4/src/config.rs
@@ -2,10 +2,9 @@ pub const USER_STACK_SIZE: usize = 4096 * 2;
 pub const KERNEL_STACK_SIZE: usize = 4096 * 2;
 pub const KERNEL_HEAP_SIZE: usize = 0x30_0000;
 pub const MEMORY_END: usize = 0x80800000;
-pub const PAGE_SIZE: usize = 0x1000;
 
 pub const TRAP_CONTEXT_END: usize = 0xfffffffffffff000;
-pub const TRAP_CONTEXT: usize = TRAP_CONTEXT_END - PAGE_SIZE;
+pub const TRAP_CONTEXT: usize = 0xffffffffffffe000;
 pub const 内核栈栈顶: usize = 0xfffffffffffff000;
 pub const 内核栈栈底: usize = 内核栈栈顶 - KERNEL_STACK_SIZE;
 
