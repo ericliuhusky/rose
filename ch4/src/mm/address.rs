816348a8bcd3c7b26ff27bd20061120003f71fbd
EricLiu <289471838@qq.com>
2023-02-22 18:06:11 +0800

    t

--- ch4/src/mm/address.rs
+++ ch4/src/mm/address.rs
@@ -9,10 +9,10 @@ pub struct 虚拟页(pub usize);
 
 
 pub fn 将地址转为页号并向下取整(v: usize) -> usize {
-    对齐到分页向下取整(v) >> 12
+    将地址转为页号(对齐到分页向下取整(v))
 }
 pub fn 将地址转为页号并向上取整(v: usize) -> usize {
-    对齐到分页向上取整(v) >> 12
+    将地址转为页号(对齐到分页向上取整(v))
 }
 pub fn 将地址转为页号(地址: usize) -> usize {
     地址 >> 12
