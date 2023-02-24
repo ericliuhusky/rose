e2a8dfe3f376b374d5d8a8494c2ed98dcccbf543
EricLiu <289471838@qq.com>
2023-02-24 17:34:21 +0800

    t

--- ch4/src/mm/memory_set.rs
+++ ch4/src/mm/memory_set.rs
@@ -3,7 +3,6 @@
 use crate::mm::page_table::多级页表;
 use crate::config::{可用物理内存结尾地址, TRAP_CONTEXT, TRAP_CONTEXT_END, 内核栈栈底, 内核栈栈顶};
 use core::arch::asm;
-use core::ops::Range;
 use crate::mm::elf_reader::Elf文件;
 use super::map_area::逻辑段;
 use super::page_table::页表;
@@ -28,53 +27,49 @@ pub struct 地址空间 {
 }
 
 impl 地址空间 {
-    fn 映射(&mut self, 虚拟地址范围: Range<usize>) {
-        for 虚拟页号 in (逻辑段 { 虚拟地址范围 }).虚拟页号范围() {
+    fn 映射(&mut self, 逻辑段: 逻辑段) {
+        for 虚拟页号 in 逻辑段.虚拟页号范围() {
             let 物理页号 = 物理内存管理器::分配物理页并返回页号();
             self.多级页表.映射(虚拟页号, 物理页号, false);
         }
     }
-    fn 用户可见映射(&mut self, 虚拟地址范围: Range<usize>) {
-        for 虚拟页号 in (逻辑段 { 虚拟地址范围 }).虚拟页号范围() {
+    fn 用户可见映射(&mut self, 逻辑段: 逻辑段) {
+        for 虚拟页号 in 逻辑段.虚拟页号范围() {
             let 物理页号 = 物理内存管理器::分配物理页并返回页号();
             self.多级页表.映射(虚拟页号, 物理页号, true);
         }
     }
-    fn 恒等映射(&mut self, 虚拟地址范围: Range<usize>) {
-        for 虚拟页号 in (逻辑段 { 虚拟地址范围 }).虚拟页号范围() {
+    fn 恒等映射(&mut self, 逻辑段: 逻辑段) {
+        for 虚拟页号 in 逻辑段.虚拟页号范围() {
             let 物理页号 = 虚拟页号;
             self.多级页表.映射(虚拟页号, 物理页号, false);
         }
     }
 
     fn 新建内核地址空间() -> Self {
-        let 逻辑段范围列表 = [
-            stext as usize..etext as usize,
-            srodata as usize..erodata as usize,
-            sdata as usize..edata as usize,
-            sbss_with_stack as usize..ebss as usize,
-            ekernel as usize..可用物理内存结尾地址,
-            0x100000..0x102000, // MMIO VIRT_TEST/RTC  in virt machine
-        ];
         let mut 地址空间 = Self { 多级页表: 多级页表::新建() };
-        for 逻辑段范围 in 逻辑段范围列表 {
-            地址空间.恒等映射(逻辑段范围);
-        }
+
+        地址空间.恒等映射(逻辑段 { 虚拟地址范围: stext as usize..etext as usize });
+        地址空间.恒等映射(逻辑段 { 虚拟地址范围: srodata as usize..erodata as usize });
+        地址空间.恒等映射(逻辑段 { 虚拟地址范围: sdata as usize..edata as usize });
+        地址空间.恒等映射(逻辑段 { 虚拟地址范围: sbss_with_stack as usize..ebss as usize });
+        地址空间.恒等映射(逻辑段 { 虚拟地址范围: ekernel as usize..可用物理内存结尾地址 });
+        地址空间.恒等映射(逻辑段 { 虚拟地址范围: 0x100000..0x102000 }); // MMIO VIRT_TEST/RTC  in virt machine
         // 内核栈
-        地址空间.映射(内核栈栈底..内核栈栈顶);
+        地址空间.映射(逻辑段 { 虚拟地址范围: 内核栈栈底..内核栈栈顶 });
         地址空间
     }
     
     pub fn 新建应用地址空间(elf文件数据: &[u8]) -> (多级页表, usize, usize) {
         let mut 地址空间 = Self { 多级页表: 多级页表::新建() };
-        // 将__trap_entry映射到用户地址空间，并使之与内核地址空间的地址相同
-        地址空间.恒等映射(__trap_entry as usize..__trap_end as usize);
 
-        // map program headers of elf, with U flag
+        // 将__trap_entry映射到用户地址空间，并使之与内核地址空间的地址相同
+        地址空间.恒等映射(逻辑段 { 虚拟地址范围: __trap_entry as usize..__trap_end as usize });
+
         let elf文件 = Elf文件::解析(elf文件数据);
         let 程序段列表 = elf文件.程序段列表();
         for 程序段 in &程序段列表 {
-            地址空间.用户可见映射(程序段.虚拟地址范围());
+            地址空间.用户可见映射(逻辑段 { 虚拟地址范围: 程序段.虚拟地址范围() });
             地址空间.多级页表.write(程序段.虚拟地址范围(), 程序段.数据);
         }
 
@@ -82,9 +77,9 @@ impl 地址空间 {
 
         let 用户栈栈底 = 逻辑段 { 虚拟地址范围: 最后一个程序段的虚拟地址范围 }.对齐到分页的结尾地址();
         let 用户栈栈顶 = 用户栈栈底 + 0x2000;
-        地址空间.用户可见映射(用户栈栈底..用户栈栈顶);
+        地址空间.用户可见映射(逻辑段 { 虚拟地址范围: 用户栈栈底..用户栈栈顶 });
         // map TrapContext
-        地址空间.映射(TRAP_CONTEXT..TRAP_CONTEXT_END);
+        地址空间.映射(逻辑段 { 虚拟地址范围: TRAP_CONTEXT..TRAP_CONTEXT_END });
         (
             地址空间.多级页表,
             用户栈栈顶,
