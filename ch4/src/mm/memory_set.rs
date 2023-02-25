8e45758339f67472168e5b9d1a509d9740b944fc
EricLiu <289471838@qq.com>
2023-02-25 14:15:06 +0800

    t

--- ch4/src/mm/memory_set.rs
+++ ch4/src/mm/memory_set.rs
@@ -101,7 +101,7 @@ impl 地址空间 {
 }
 
 impl 地址空间 {
-    pub fn 应用地址空间的上下文(&self) -> &'static mut 陷入上下文 {
+    pub fn 陷入上下文(&self) -> &'static mut 陷入上下文 {
         let pa_ranges = self.多级页表.虚拟地址范围转换物理地址范围列表(应用陷入上下文存放地址()..0xfffffffffffff000);
         unsafe {
             &mut *(pa_ranges[0].start as *mut 陷入上下文)
