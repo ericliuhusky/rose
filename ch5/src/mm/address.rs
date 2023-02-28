eb48ecf49e802f9c809ec03d7529470c40bbce52
EricLiu <289471838@qq.com>
2023-02-28 22:20:01 +0800

    t

--- ch5/src/mm/address.rs
+++ ch5/src/mm/address.rs
@@ -31,12 +31,11 @@ impl 内存分页 {
 
 use core::ops::Range;
 
-/// 一段连续地址的虚拟内存
-pub struct 逻辑段 {
-    pub 虚拟地址范围: Range<usize>
+pub struct 连续地址虚拟内存 {
+    pub 虚拟地址范围: Range<usize>,
 }
 
-impl 逻辑段 {
+impl 连续地址虚拟内存 {
     pub fn 虚拟页号范围(&self) -> Range<usize> {
         let 起始页号 = 内存地址(self.虚拟地址范围.start).对齐到分页向下取整().页号();
         let 结尾页号 = 内存地址(self.虚拟地址范围.end).对齐到分页向上取整().页号();
@@ -46,4 +45,10 @@ impl 逻辑段 {
     pub fn 对齐到分页的结尾地址(&self) -> usize {
         内存地址(self.虚拟地址范围.end).对齐到分页向上取整().0
     }
+}
+
+pub struct 逻辑段 {
+    pub 连续地址虚拟内存: 连续地址虚拟内存,
+    pub 恒等映射: bool,
+    pub 用户可见: bool
 }
--- ch5/src/task/mod.rs
+++ ch5/src/task/mod.rs
@@ -1,37 +1,11 @@
+mod task;
+mod pid;
+
 use crate::loader::{读取应用数目, 读取应用数据};
 use alloc::vec::Vec;
 use crate::格式化输出并换行;
 use crate::终止::终止;
-use crate::mm::memory_set::{地址空间, 内核地址空间};
-use crate::trap::陷入上下文;
-
-pub struct 任务 {
-    状态: 任务状态,
-    pub 地址空间: 地址空间,
-}
-
-impl 任务 {
-    fn 新建(elf文件数据: &[u8]) -> Self {
-        let (地址空间, 用户栈栈顶, 应用入口地址) = 地址空间::新建应用地址空间(elf文件数据);
-        let 上下文 = 地址空间.陷入上下文();
-        *上下文 = 陷入上下文::应用初始上下文(
-            应用入口地址,
-            用户栈栈顶,
-            unsafe { 内核地址空间.token() },
-        );
-        Self {
-            状态: 任务状态::就绪,
-            地址空间
-        }
-    }
-}
-
-#[derive(PartialEq)]
-enum 任务状态 {
-    就绪,
-    运行,
-    终止,
-}
+use self::task::{任务, 任务状态};
 
 
 pub struct 任务管理器 {
