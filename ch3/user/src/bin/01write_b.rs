312fc64c8c7a66bf37f9bad52cd8601ca7fcb123
EricLiu <289471838@qq.com>
2023-03-04 12:59:26 +0800

    t

--- ch3/user/src/bin/01write_b.rs
+++ ch3/user/src/bin/01write_b.rs
@@ -1,15 +1,15 @@
 #![no_std]
 #![no_main]
 
+#[macro_use]
 extern crate user;
-use user::格式化输出并换行;
 use user::fibonacci;
 
 #[no_mangle]
 fn main() -> i32 {
     for i in 1..=30 {
         let r = fibonacci(i);
-        格式化输出并换行!("B [{}/{}] {}", i, 30, r);
+        println!("B [{}/{}] {}", i, 30, r);
     }
     0
 }
--- ch3/user/src/lib.rs
+++ ch3/user/src/lib.rs
@@ -1,15 +1,17 @@
 #![no_std]
 
-use 终止::终止;
+use print::println;
+use sys_call::exit;
+use panic;
 
 #[no_mangle]
 #[link_section = ".text.entry"]
 fn _start() {
     extern "C" {
-        fn main() -> i32;
+        fn main() -> isize;
     }
     let 终止代码 = unsafe { main() };
-    终止(终止代码);
+    exit(终止代码);
 }
 
 // 一个耗时程序，用以验证10ms之后会自动切换下一个任务
@@ -17,63 +19,4 @@ pub fn fibonacci(x: u32) -> u32 {
     if x == 0 { return 0 }
     if x == 1 { return 1 }
     fibonacci(x - 2) + fibonacci(x - 1)
-}
-
-
-pub mod 输出 {
-    use core::arch::asm;
-    use core::fmt::{self, Write};
-
-    fn 输出(字符串: &str) {
-        unsafe {
-            asm!(
-                "ecall",
-                in("x10") 字符串.as_bytes().as_ptr() as usize,
-                in("x11") 字符串.as_bytes().len(),
-                in("x17") 1
-            );
-        }
-    }
-
-    struct 标准输出;
-    impl Write for 标准输出 {
-        fn write_str(&mut self, s: &str) -> fmt::Result {
-            输出(s);
-            Ok(())
-        }
-    }
-    pub fn 格式化输出(参数: fmt::Arguments) {
-        标准输出.write_fmt(参数).unwrap();
-    }
-
-    #[macro_export]
-    macro_rules! 格式化输出并换行 {
-        ($fmt: literal $(, $($arg: tt)+)?) => {
-            $crate::输出::格式化输出(format_args!(concat!($fmt, "\n") $(, $($arg)+)?));
-        }
-    }
-}
-
-mod 终止 {
-    use core::arch::asm;
-
-    pub fn 终止(代码: i32) {
-        unsafe {
-            asm!(
-                "ecall",
-                in("x10") 代码 as usize,
-                in("x17") 2
-            );
-        }
-    }
-}
-
-mod rust裸机无标准库 {
-    use core::panic::PanicInfo;
-
-    // 需要提供崩溃处理
-    #[panic_handler]
-    fn panic(_info: &PanicInfo) -> ! {
-        loop {}
-    }
 }
