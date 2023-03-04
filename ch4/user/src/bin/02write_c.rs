3d4841af8338bf3e89d5d4f54d9fb1985504f0f2
EricLiu <289471838@qq.com>
2023-03-04 14:31:15 +0800

    t

--- ch4/user/src/bin/02write_c.rs
+++ ch4/user/src/bin/02write_c.rs
@@ -2,15 +2,19 @@
 #![no_main]
 
 #[macro_use]
-extern crate user;
-
-use user::fibonacci;
+extern crate lib;
 
 #[no_mangle]
-fn main() -> i32 {
+fn main() {
     for i in 1..=30 {
         let r = fibonacci(i);
         println!("C [{}/{}] {}", i, 30, r);
     }
-    0
+}
+
+// 一个耗时程序，用以验证10ms之后会自动切换下一个任务
+pub fn fibonacci(x: u32) -> u32 {
+    if x == 0 { return 0 }
+    if x == 1 { return 1 }
+    fibonacci(x - 2) + fibonacci(x - 1)
 }
