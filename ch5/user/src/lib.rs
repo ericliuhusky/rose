f7bba1b19a10f28f5fd27ae9c812efc431adf94b
EricLiu <289471838@qq.com>
2023-03-05 12:49:59 +0800

    t

--- ch5/user/src/lib.rs
+++ ch5/user/src/lib.rs
@@ -16,12 +16,6 @@ fn _start() {
     exit(终止代码);
 }
 
-// 一个耗时程序，用以验证10ms之后会自动切换下一个任务
-pub fn fibonacci(x: u32) -> u32 {
-    if x == 0 { return 0 }
-    if x == 1 { return 1 }
-    fibonacci(x - 2) + fibonacci(x - 1)
-}
 
 use core::arch::asm;
 
