bf81a5e595e72c0de3f5618bfd87d4a311302eee
EricLiu <289471838@qq.com>
2023-03-05 12:47:33 +0800

    t

--- ch5/user/src/lib.rs
+++ ch5/user/src/lib.rs
@@ -25,87 +25,17 @@ pub fn fibonacci(x: u32) -> u32 {
 
 use core::arch::asm;
 
-const SYSCALL_READ: usize = 0;
-const SYSCALL_WRITE: usize = 1;
-const SYSCALL_EXIT: usize = 2;
-const SYSCALL_YIELD: usize = 3;
-const SYSCALL_GET_TIME: usize = 4;
-const SYSCALL_GETPID: usize = 5;
-const SYSCALL_FORK: usize = 6;
-const SYSCALL_EXEC: usize = 7;
-const SYSCALL_WAITPID: usize = 8;
-
-fn syscall(id: usize, args: [usize; 3]) -> isize {
-    let mut ret: isize;
-    unsafe {
-        asm!(
-            "ecall",
-            inlateout("x10") args[0] => ret,
-            in("x11") args[1],
-            in("x12") args[2],
-            in("x17") id
-        );
-    }
-    ret
-}
-
-pub fn sys_read(buffer: &mut [u8]) -> isize {
-    syscall(
-        SYSCALL_READ,
-        [buffer.as_mut_ptr() as usize, buffer.len(), 0],
-    )
-}
-
-pub fn sys_yield() -> isize {
-    syscall(SYSCALL_YIELD, [0, 0, 0])
-}
-
-pub fn sys_get_time() -> isize {
-    syscall(SYSCALL_GET_TIME, [0, 0, 0])
-}
-
-pub fn sys_getpid() -> isize {
-    syscall(SYSCALL_GETPID, [0, 0, 0])
-}
-
-pub fn sys_fork() -> isize {
-    syscall(SYSCALL_FORK, [0, 0, 0])
-}
-
-pub fn sys_exec(path: &str) -> isize {
-    syscall(SYSCALL_EXEC, [path.as_ptr() as usize, path.len(), 0])
-}
-
-pub fn sys_waitpid(pid: isize, exit_code: *mut i32) -> isize {
-    syscall(SYSCALL_WAITPID, [pid as usize, exit_code as usize, 0])
-}
-
 pub fn getchar() -> u8 {
     let mut c = [0u8; 1];
     read(&mut c);
     c[0]
 }
-pub fn read(buf: &mut [u8]) -> isize {
-    sys_read(buf)
-}
-pub fn yield_() -> isize {
-    sys_yield()
-}
-pub fn get_time() -> isize {
-    sys_get_time()
-}
-pub fn getpid() -> isize {
-    sys_getpid()
-}
-pub fn fork() -> isize {
-    sys_fork()
-}
-pub fn exec(path: &str) -> isize {
-    sys_exec(path)
-}
+
+pub use sys_call::{read, yield_, get_time, getpid, fork, exec};
+
 pub fn wait(exit_code: &mut i32) -> isize {
     loop {
-        match sys_waitpid(-1, exit_code as *mut _) {
+        match sys_call::waitpid(-1, exit_code as *mut _) {
             -2 => {
                 yield_();
             }
@@ -117,7 +47,7 @@ pub fn wait(exit_code: &mut i32) -> isize {
 
 pub fn waitpid(pid: usize, exit_code: &mut i32) -> isize {
     loop {
-        match sys_waitpid(pid as isize, exit_code as *mut _) {
+        match sys_call::waitpid(pid as isize, exit_code as *mut _) {
             -2 => {
                 yield_();
             }
@@ -127,8 +57,8 @@ pub fn waitpid(pid: usize, exit_code: &mut i32) -> isize {
     }
 }
 pub fn sleep(period_ms: usize) {
-    let start = sys_get_time();
-    while sys_get_time() < start + period_ms as isize {
-        sys_yield();
+    let start = get_time();
+    while get_time() < start + period_ms as isize {
+        yield_();
     }
 }
--- user_crates/sys_call/src/lib.rs
+++ user_crates/sys_call/src/lib.rs
@@ -17,8 +17,20 @@ fn sys_call(id: usize, args: [usize; 3]) -> isize {
     ret
 }
 
-const SYS_PUTCHAR: usize = 9;
+const SYS_READ: usize = 0;
+const SYS_WRITE: usize = 1;
 const SYS_EXIT: usize = 2;
+const SYS_YIELD: usize = 3;
+const SYS_GET_TIME: usize = 4;
+const SYS_GETPID: usize = 5;
+const SYS_FORK: usize = 6;
+const SYS_EXEC: usize = 7;
+const SYS_WAITPID: usize = 8;
+const SYS_PUTCHAR: usize = 9;
+
+pub fn read(buffer: &mut [u8]) -> isize {
+    sys_call(SYS_READ, [buffer as *mut [u8] as *mut u8 as usize, buffer.len(), 0])
+}
 
 pub fn putchar(c: usize) {
     sys_call(SYS_PUTCHAR, [c, 0, 0]);
@@ -27,4 +39,28 @@ pub fn putchar(c: usize) {
 pub fn exit(exit_code: isize) -> ! {
     sys_call(SYS_EXIT, [exit_code as usize, 0, 0]);
     panic!("exit")
+}
+
+pub fn yield_() -> isize {
+    sys_call(SYS_YIELD, [0, 0, 0])
+}
+
+pub fn get_time() -> isize {
+    sys_call(SYS_GET_TIME, [0, 0, 0])
+}
+
+pub fn getpid() -> isize {
+    sys_call(SYS_GETPID, [0, 0, 0])
+}
+
+pub fn fork() -> isize {
+    sys_call(SYS_FORK, [0, 0, 0])
+}
+
+pub fn exec(path: &str) -> isize {
+    sys_call(SYS_EXEC, [path.as_ptr() as usize, path.len(), 0])
+}
+
+pub fn waitpid(pid: isize, exit_code: *mut i32) -> isize {
+    sys_call(SYS_WAITPID, [pid as usize, exit_code as usize, 0])
 }
