d3f7ea412089576ef8fa0b75e2f4da9469576ddb
EricLiu <289471838@qq.com>
2023-03-05 23:30:24 +0800

    t

--- make/src/main.rs
+++ make/src/main.rs
@@ -9,9 +9,9 @@ fn clean(dir: &str) {
         .current_dir(dir)
         .arg("clean")
         .spawn()
-        .expect("clean")
+        .unwrap()
         .wait()
-        .expect("wait clean");
+        .unwrap();
 }
 
 fn build(dir: &str, config: Option<&str>, nightly: bool, bin: Option<&str>) {
@@ -22,19 +22,11 @@ fn build(dir: &str, config: Option<&str>, nightly: bool, bin: Option<&str>) {
     if let Some(config) = config {
         cmd.args(["--config", config]);
     }
-    cmd
-        .current_dir(dir) 
-        .arg("build")
-        .args(["--target", TARGET]);
+    cmd.current_dir(dir).arg("build").args(["--target", TARGET]);
     if let Some(bin) = bin {
         cmd.args(["--bin", bin]);
     }
-    cmd
-        .arg("--release")
-        .spawn()
-        .expect("build")
-        .wait()
-        .expect("wait build");
+    cmd.arg("--release").spawn().unwrap().wait().unwrap();
 }
 
 fn elf_to_bin(dir: &str, kernel_elf: &str, kernel_bin: &str) {
@@ -44,9 +36,9 @@ fn elf_to_bin(dir: &str, kernel_elf: &str, kernel_bin: &str) {
         .arg("--strip-all")
         .args(["-O", "binary", kernel_bin])
         .spawn()
-        .expect("elf_to_bin")
+        .unwrap()
         .wait()
-        .expect("wait elf_to_bin");
+        .unwrap();
 }
 
 fn qemu_run(dir: &str, kernel_bin: &str) -> String {
@@ -55,37 +47,42 @@ fn qemu_run(dir: &str, kernel_bin: &str) -> String {
         .args(["-machine", "virt"])
         .arg("-nographic")
         .args(["-bios", BOOTLOADER])
-        .args(["-device", &format!("loader,file={},addr={}", kernel_bin, KERNEL_ENTRY)])
+        .args([
+            "-device",
+            &format!("loader,file={},addr={}", kernel_bin, KERNEL_ENTRY),
+        ])
         .output()
-        .expect("qemu_run")
+        .unwrap()
         .stdout
-        .iter().map(|b| *b as char).collect()
+        .iter()
+        .map(|b| *b as char)
+        .collect()
 }
 
 struct Makefile {
     link_arg: &'static str,
     nightly: bool,
     dir: &'static str,
-    users: Option<[User; 3]>
+    users: Option<[User; 3]>,
 }
 
 struct User {
     bin: &'static str,
-    enrty: Option<usize>
+    enrty: Option<usize>,
 }
 
 const CH0: Makefile = Makefile {
     link_arg: "-Ttext=0x80200000",
     nightly: false,
     dir: "../ch0",
-    users: None
+    users: None,
 };
 
 const CH1: Makefile = Makefile {
     link_arg: "-Tsrc/linker.ld",
     nightly: true,
     dir: "../ch1",
-    users: None
+    users: None,
 };
 
 const CH2: Makefile = Makefile {
@@ -95,17 +92,17 @@ const CH2: Makefile = Makefile {
     users: Some([
         User {
             bin: "hello_world",
-            enrty: Some(0x80400000)
+            enrty: Some(0x80400000),
         },
         User {
             bin: "priv_inst",
-            enrty: Some(0x80400000)
+            enrty: Some(0x80400000),
         },
         User {
             bin: "store_fault",
-            enrty: Some(0x80400000)
-        }
-    ])
+            enrty: Some(0x80400000),
+        },
+    ]),
 };
 
 const CH3: Makefile = Makefile {
@@ -115,17 +112,17 @@ const CH3: Makefile = Makefile {
     users: Some([
         User {
             bin: "00write_a",
-            enrty: Some(0x80600000)
+            enrty: Some(0x80600000),
         },
         User {
             bin: "01write_b",
-            enrty: Some(0x80620000)
+            enrty: Some(0x80620000),
         },
         User {
             bin: "02write_c",
-            enrty: Some(0x80640000)
-        }
-    ])
+            enrty: Some(0x80640000),
+        },
+    ]),
 };
 
 const CH4: Makefile = Makefile {
@@ -135,17 +132,17 @@ const CH4: Makefile = Makefile {
     users: Some([
         User {
             bin: "00write_a",
-            enrty: None
+            enrty: None,
         },
         User {
             bin: "01write_b",
-            enrty: None
+            enrty: None,
         },
         User {
             bin: "02write_c",
-            enrty: None
-        }
-    ])
+            enrty: None,
+        },
+    ]),
 };
 
 fn main() {
@@ -158,10 +155,9 @@ fn main() {
         "ch2" => CH2,
         "ch3" => CH3,
         "ch4" => CH4,
-        _ => panic!()
+        _ => panic!(),
     };
 
-    
     let kernel_elf = format!("target/{}/release/kernel", TARGET);
     let kernel_bin = format!("{}.bin", kernel_elf);
 
@@ -170,19 +166,25 @@ fn main() {
         for user in users {
             if let Some(entry) = user.enrty {
                 let link_arg = format!("-Ttext={:x}", entry);
-                let config = format!(r#"target.{}.rustflags = ["-Clink-arg={}"]"#, TARGET, link_arg);
+                let config = format!(
+                    r#"target.{}.rustflags = ["-Clink-arg={}"]"#,
+                    TARGET, link_arg
+                );
                 build("../user", Some(&config), true, Some(user.bin));
             } else {
                 build("../user", None, true, Some(user.bin));
             }
         }
     }
-     
+
     clean(ch.dir);
-    let config = format!(r#"target.{}.rustflags = ["-Clink-arg={}"]"#, TARGET, ch.link_arg);
+    let config = format!(
+        r#"target.{}.rustflags = ["-Clink-arg={}"]"#,
+        TARGET, ch.link_arg
+    );
     build(ch.dir, Some(&config), ch.nightly, None);
     elf_to_bin(ch.dir, &kernel_elf, &kernel_bin);
-    
+
     let output = qemu_run(ch.dir, &kernel_bin);
     print!("{}", output);
 }
