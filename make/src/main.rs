d0494e6508f3b59006529738e9448fbf1587f2f2
EricLiu <289471838@qq.com>
2023-03-06 15:36:10 +0800

    t

--- make/src/main.rs
+++ make/src/main.rs
@@ -7,42 +7,28 @@ const TARGET: &str = "riscv64gc-unknown-none-elf";
 const BOOTLOADER: &str = "../rustsbi-qemu.bin";
 const KERNEL_ENTRY: &str = "0x80200000";
 
-fn clean() -> String {
-    String::from("cargo clean")
-}
-
 fn build(nightly: bool, config: Option<&str>, bin: Option<&str>) -> String {
-    let mut cmd = String::from("cargo");
-    if nightly {
-        cmd.push_str(" +nightly");
-    }
-    cmd.push_str(" build");
-    if let Some(config) = config {
-        cmd.push_str(format!(" --config '{}'", config).as_str());
-    }
-    cmd.push_str(format!(" --target {}", TARGET).as_str());
-    if let Some(bin) = bin {
-        cmd.push_str(format!(" --bin {}", bin).as_str());
-    }
-    cmd.push_str(" --release");
-    cmd
+    let nightly = if nightly { "+nightly" } else { "" };
+    let config = if let Some(config) = config {
+        format!("--config '{}'", config)
+    } else {
+        String::new()
+    };
+    let bin = if let Some(bin) = bin {
+        format!("--bin {}", bin)
+    } else {
+        String::new()
+    };
+    format!(
+        "cargo {} build {} --target {} {} --release",
+        nightly, config, TARGET, bin
+    )
 }
 
 fn elf_to_bin(kernel_elf: &str, kernel_bin: &str) -> String {
     format!(
         "rust-objcopy {} --strip-all -O binary {}",
         kernel_elf, kernel_bin
-    )
-}
-
-fn qemu_run(kernel_bin: &str) -> String {
-    format!(
-        "qemu-system-riscv64 \
-            -machine virt \
-            -nographic \
-            -bios {} \
-            -device loader,file={},addr={}",
-        BOOTLOADER, kernel_bin, KERNEL_ENTRY
     )
 }
 
@@ -59,34 +45,44 @@ fn main() {
     }
 
     for ch in ch() {
-        let mut build_user = String::new();
-        if !ch.users.is_empty() {
-            build_user.push_str("cd ../user");
-            build_user.push_str(format!(" && {}", clean()).as_str());
+        let build_user = if ch.users.is_empty() {
+            String::new()
+        } else {
+            let mut users = String::new();
             for user in &ch.users {
-                let build_cmd;
-                if let Some(entry) = user.enrty {
+                let build_cmd = if let Some(entry) = user.enrty {
                     let link_arg = format!("-Ttext={:x}", entry);
                     let config = rustflags(&link_arg);
-                    build_cmd = build(true, Some(&config), Some(user.bin));
+                    build(true, Some(&config), Some(user.bin))
                 } else {
-                    build_cmd = build(true, None, Some(user.bin));
-                }
-                build_user.push_str(format!(" && {}", build_cmd).as_str())
+                    build(true, None, Some(user.bin))
+                };
+                users.push_str(format!(" && {}", build_cmd).as_str());
             }
-        }
+            format!("cd ../user && cargo clean {}", users)
+        };
 
         let config = rustflags(ch.link_arg);
 
         let mut f = File::create(format!("{}/Makefile", ch.dir).as_str()).unwrap();
         writeln!(
             f,
-            "run:\n\t@{}\n\t@{}\n\t@{}\n\t@{}\n\t@{}\n",
+            "run:\n\
+            \t@{}\n\
+            \t@cargo clean\n\
+            \t@{}\n\
+            \t@{}\n\
+            \t@qemu-system-riscv64 \
+                -machine virt \
+                -nographic \
+                -bios {} \
+                -device loader,file={},addr={}",
             build_user,
-            clean(),
             build(ch.nightly, Some(&config), None),
             elf_to_bin(&kernel_elf, &kernel_bin),
-            qemu_run(&kernel_bin)
+            BOOTLOADER,
+            kernel_bin,
+            KERNEL_ENTRY
         )
         .unwrap();
     }
