8b4957a3b4d91d7f0fca1e6995707bce315053ef
EricLiu <289471838@qq.com>
2023-03-06 20:52:34 +0800

    t

--- make/src/main.rs
+++ make/src/main.rs
@@ -79,6 +79,51 @@ fn create_makefile(ch: &Makefile, kernel_elf: &str, kernel_bin: &str) {
     .unwrap();
 }
 
+fn create_link_app(ch: &Makefile) {
+    if ch.users.is_empty() {
+        return;
+    }
+    let mut f = File::create(format!("{}/src/link_app.s", ch.dir)).unwrap();
+    writeln!(
+        f,
+        r#"# created by crate make
+    .section .data
+    .globl _num_app
+_num_app:
+    .quad {}"#,
+        ch.users.len()
+    )
+    .unwrap();
+    for i in 0..ch.users.len() {
+        writeln!(f, r#"    .quad app_{}_start"#, i).unwrap();
+    }
+    writeln!(f, r#"    .quad app_{}_end"#, ch.users.len() - 1).unwrap();
+    writeln!(
+        f,
+        r#"
+    .globl _app_names
+_app_names:"#
+    )
+    .unwrap();
+    for user in &ch.users {
+        writeln!(f, r#"    .string "{}""#, user.bin).unwrap();
+    }
+    for (i, user) in ch.users.iter().enumerate() {
+        writeln!(
+            f,
+            r#"
+    .section .data
+    .globl app_{0}_start
+    .globl app_{0}_end
+app_{0}_start:
+    .incbin "../user/target/{1}/release/{2}"
+app_{0}_end:"#,
+            i, TARGET, user.bin
+        )
+        .unwrap();
+    }
+}
+
 fn main() {
     config::init();
     let kernel_elf = format!("target/{}/release/kernel", TARGET);
@@ -86,48 +131,6 @@ fn main() {
 
     for ch in ch() {
         create_makefile(ch, &kernel_elf, &kernel_bin);
-        if ch.users.is_empty() {
-            continue;
-        }
-        let mut f = File::create(format!("{}/src/link_app.s", ch.dir)).unwrap();
-        writeln!(
-            f,
-            r#"# created by crate make
-    .align 3
-    .section .data
-    .globl _num_app
-_num_app:
-    .quad {}"#,
-            ch.users.len()
-        )
-        .unwrap();
-        for i in 0..ch.users.len() {
-            writeln!(f, r#"    .quad app_{}_start"#, i).unwrap();
-        }
-        writeln!(f, r#"    .quad app_{}_end"#, ch.users.len() - 1).unwrap();
-        writeln!(
-            f,
-            r#"
-    .globl _app_names
-_app_names:"#
-        )
-        .unwrap();
-        for user in &ch.users {
-            writeln!(f, r#"    .string "{}""#, user.bin).unwrap();
-        }
-        for (i, user) in ch.users.iter().enumerate() {
-            writeln!(
-                f,
-                r#"
-    .section .data
-    .globl app_{0}_start
-    .globl app_{0}_end
-app_{0}_start:
-    .incbin "../user/target/{1}/release/{2}"
-app_{0}_end:"#,
-                i, TARGET, user.bin
-            )
-            .unwrap();
-        }
+        create_link_app(ch);
     }
 }
