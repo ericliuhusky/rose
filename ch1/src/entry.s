dea3cbedeb0eb093d13c972d64acc5655b8cfd77
EricLiu <289471838@qq.com>
2023-03-07 11:23:21 +0800

    t

--- ch1/src/entry.s
+++ ch1/src/entry.s
@@ -1,12 +1,8 @@
-    .section .text
     .globl _start
 _start:
-    la sp, boot_stack_top
-    call rust_main
+    la sp, __BOOT_STACK_TOP
+    call main
 
-    .section .data
-    .globl boot_stack
-boot_stack:
-    .space 4096 * 16
-    .globl boot_stack_top
-boot_stack_top:
+__BOOT_STACK:
+    .space 0x10000
+__BOOT_STACK_TOP:
--- ch1/src/main.rs
+++ ch1/src/main.rs
@@ -9,15 +9,7 @@ use panic;
 global_asm!(include_str!("entry.s"));
 
 #[no_mangle]
-fn rust_main() {
-    extern "C" {
-        fn boot_stack(); // 栈底
-        fn boot_stack_top(); // 栈顶
-    }
-    println!(
-        "boot_stack [{:#x}, {:#x})",
-        boot_stack as usize, boot_stack_top as usize
-    );
+fn main() {
     println!("Hello, world!");
     shutdown();
 }
