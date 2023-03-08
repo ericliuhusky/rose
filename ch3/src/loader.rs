19ea31c75b456f4b901d67cc875cacf8d241860d
EricLiu <289471838@qq.com>
2023-03-08 10:37:31 +0800

    t

--- ch3/src/loader.rs
+++ ch3/src/loader.rs
@@ -1,10 +1,5 @@
 use crate::trap::陷入上下文;
 
-const 用户栈栈顶: [usize; 3] = [
-    0x80422000,
-    0x80444000,
-    0x80466000
-];
 const 内核栈栈顶: [usize; 3] = [
     0x80468000,
     0x8046a000,
@@ -22,7 +17,7 @@ fn 将上下文压入内核栈后的栈顶(上下文: 陷入上下文, 应用索
 }
 
 
-fn 加载应用到应用内存区(应用索引: usize) -> usize {
+fn 加载应用到应用内存区(应用索引: usize) -> (usize, usize) {
     unsafe {
         let 应用数据 = loader::read_app_data(应用索引);
         let elf = elf_reader::ElfFile::read(应用数据);
@@ -41,17 +36,18 @@ fn 加载应用到应用内存区(应用索引: usize) -> usize {
                 dst[j] = src[j];
             }
         }
-        elf.entry_address()
+        let last_p_va_end = elf.programs().last().unwrap().virtual_address_range().end;
+        let user_stack_top = last_p_va_end +0x2000;
+        (elf.entry_address(), user_stack_top)
     }
 }
 
 pub fn 将应用初始上下文压入内核栈后的栈顶(应用索引: usize) -> usize {
-    let ea = 加载应用到应用内存区(应用索引);
-    println!("{:x}", ea);
+    let (entry_address, user_stack_top) = 加载应用到应用内存区(应用索引);
     将上下文压入内核栈后的栈顶(
         陷入上下文::应用初始上下文(
-            ea,
-            用户栈栈顶[应用索引]
+            entry_address,
+            user_stack_top
         ),
         应用索引
     )
