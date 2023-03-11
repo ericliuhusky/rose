479774217999b19caac4c6713c7f7b877a776c91
EricLiu <289471838@qq.com>
2023-03-11 17:17:26 +0800

    :fire: remove unused

--- fs/src/block_cache.rs
+++ fs/src/block_cache.rs
@@ -1,7 +1,6 @@
 use super::{BlockDevice, BLOCK_SZ};
-use alloc::collections::VecDeque;
+use alloc::vec::Vec;
 use alloc::sync::Arc;
-use lazy_static::*;
 use spin::Mutex;
 /// Cached block inside memory
 pub struct BlockCache {
@@ -27,38 +26,16 @@ impl BlockCache {
             modified: false,
         }
     }
-    /// Get the address of an offset inside the cached block data
-    fn addr_of_offset(&self, offset: usize) -> usize {
-        &self.cache[offset] as *const _ as usize
-    }
-
-    pub fn get_ref<T>(&self, offset: usize) -> &T
-    where
-        T: Sized,
-    {
-        let type_size = core::mem::size_of::<T>();
-        assert!(offset + type_size <= BLOCK_SZ);
-        let addr = self.addr_of_offset(offset);
-        unsafe { &*(addr as *const T) }
-    }
-
-    pub fn get_mut<T>(&mut self, offset: usize) -> &mut T
-    where
-        T: Sized,
-    {
-        let type_size = core::mem::size_of::<T>();
-        assert!(offset + type_size <= BLOCK_SZ);
-        self.modified = true;
-        let addr = self.addr_of_offset(offset);
-        unsafe { &mut *(addr as *mut T) }
-    }
 
     pub fn read<T, V>(&self, offset: usize, f: impl FnOnce(&T) -> V) -> V {
-        f(self.get_ref(offset))
+        let addr = &self.cache[offset] as *const u8 as usize;
+        f(unsafe { &*(addr as *const T) })
     }
 
     pub fn modify<T, V>(&mut self, offset: usize, f: impl FnOnce(&mut T) -> V) -> V {
-        f(self.get_mut(offset))
+        self.modified = true;
+        let addr = &self.cache[offset] as *const u8 as usize;
+        f(unsafe { &mut *(addr as *mut T) })
     }
 
     pub fn sync(&mut self) {
@@ -78,16 +55,10 @@ impl Drop for BlockCache {
 const BLOCK_CACHE_SIZE: usize = 16;
 
 pub struct BlockCacheManager {
-    queue: VecDeque<(usize, Arc<Mutex<BlockCache>>)>,
+    queue: Vec<(usize, Arc<Mutex<BlockCache>>)>,
 }
 
 impl BlockCacheManager {
-    pub fn new() -> Self {
-        Self {
-            queue: VecDeque::new(),
-        }
-    }
-
     pub fn get_block_cache(
         &mut self,
         block_id: usize,
@@ -105,7 +76,7 @@ impl BlockCacheManager {
                     .enumerate()
                     .find(|(_, pair)| Arc::strong_count(&pair.1) == 1)
                 {
-                    self.queue.drain(idx..=idx);
+                    self.queue.remove(idx);
                 } else {
                     panic!("Run out of BlockCache!");
                 }
@@ -115,30 +86,29 @@ impl BlockCacheManager {
                 block_id,
                 Arc::clone(&block_device),
             )));
-            self.queue.push_back((block_id, Arc::clone(&block_cache)));
+            self.queue.push((block_id, Arc::clone(&block_cache)));
             block_cache
         }
     }
 }
 
-lazy_static! {
-    /// The global block cache manager
-    pub static ref BLOCK_CACHE_MANAGER: Mutex<BlockCacheManager> =
-        Mutex::new(BlockCacheManager::new());
-}
+static mut BLOCK_CACHE_MANAGER: BlockCacheManager = BlockCacheManager {
+    queue: Vec::new(),
+};
 /// Get the block cache corresponding to the given block id and block device
 pub fn get_block_cache(
     block_id: usize,
     block_device: Arc<dyn BlockDevice>,
 ) -> Arc<Mutex<BlockCache>> {
-    BLOCK_CACHE_MANAGER
-        .lock()
-        .get_block_cache(block_id, block_device)
+    unsafe {
+        BLOCK_CACHE_MANAGER.get_block_cache(block_id, block_device)
+    }
 }
 /// Sync all block cache to block device
 pub fn block_cache_sync_all() {
-    let manager = BLOCK_CACHE_MANAGER.lock();
-    for (_, cache) in manager.queue.iter() {
-        cache.lock().sync();
+    unsafe {
+        for (_, cache) in BLOCK_CACHE_MANAGER.queue.iter() {
+            cache.lock().sync();
+        }
     }
 }
