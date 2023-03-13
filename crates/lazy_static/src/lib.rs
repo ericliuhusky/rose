146beb2870ceb748b22508922105aff1916e5b0b
EricLiu <289471838@qq.com>
2023-03-13 14:41:56 +0800

    :passport_control: pub and priv

--- crates/lazy_static/src/lib.rs
+++ crates/lazy_static/src/lib.rs
@@ -2,11 +2,22 @@
 
 pub mod once;
 
-#[macro_export]
+#[macro_export(local_inner_macros)]
 macro_rules! lazy_static {
     (pub static $N:ident : $T:ty = $e:expr;) => {
-        pub struct $N {}
-        pub static $N: $N = $N {};
+        __lazy_static!((pub) static $N : $T = $e;);
+    };
+    (static $N:ident : $T:ty = $e:expr;) => {
+        __lazy_static!(() static $N : $T = $e;);
+    }
+}
+
+#[macro_export(local_inner_macros)]
+macro_rules! __lazy_static {
+    (($($vis:tt)*) static $N:ident : $T:ty = $e:expr;) => {
+        #[allow(non_camel_case_types)]
+        $($vis)* struct $N {}
+        $($vis)* static $N: $N = $N {};
         impl core::ops::Deref for $N {
             type Target = $T;
             fn deref(&self) -> &$T {
