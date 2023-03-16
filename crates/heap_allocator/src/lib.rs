e825aa57afbe25125376d9eaa1ad677a68c3585e
EricLiu <289471838@qq.com>
2023-03-16 12:39:05 +0800

    :ambulance: ch6 HEAP_SIZE is not big enough

--- crates/heap_allocator/src/lib.rs
+++ crates/heap_allocator/src/lib.rs
@@ -3,18 +3,21 @@
 
 use buddy_system_allocator::LockedHeap;
 
-// sys 0x30_0000
+#[cfg(feature = "user")]
 const HEAP_SIZE: usize = 0x4000;
+#[cfg(not(feature = "user"))]
+const HEAP_SIZE: usize = 0x200_0000;
 
 static mut HEAP: [u8; HEAP_SIZE] = [0; HEAP_SIZE];
 
 #[global_allocator]
-static HEAP_ALLOCATOR: LockedHeap = LockedHeap::empty();
+static mut HEAP_ALLOCATOR: LockedHeap = LockedHeap::empty();
 
 pub fn init() {
     unsafe {
         HEAP_ALLOCATOR
-            .lock()
+            .0
+            .borrow_mut()
             .init(&HEAP as *const [u8] as *const u8 as usize, HEAP_SIZE);
     }
 }
