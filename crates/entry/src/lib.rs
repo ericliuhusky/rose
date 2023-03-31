2355b7820373ba30558b67da8807baac360e8ea1
EricLiu <289471838@qq.com>
2023-03-31 23:37:30 +0800

    :art: asm -> inline

--- crates/entry/src/lib.rs
+++ crates/entry/src/lib.rs
@@ -1,5 +1,17 @@
 #![no_std]
+#![feature(linkage)]
 
-use core::arch::global_asm;
+use core::arch::asm;
 
-global_asm!(include_str!("entry.s"));
+#[no_mangle]
+#[link_section = ".text.entry"]
+fn _start() {
+    let top = BOOT_STACK.as_ptr() as usize + BOOT_STACK.len();
+    unsafe {
+        asm!("mv sp, {}", in(reg) top);
+        asm!("call main")
+    }
+}
+
+#[link_section = ".data"]
+static BOOT_STACK: [u8; 0x10000] = [0; 0x10000];
