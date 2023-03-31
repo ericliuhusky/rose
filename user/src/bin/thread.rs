dad3d3a3afc1755a98d5a4f458e4afc33916bc57
EricLiu <289471838@qq.com>
2023-03-31 23:24:02 +0800

    :white_check_mark: 更改测试内核线程的程序

--- user/src/bin/thread.rs
+++ user/src/bin/thread.rs
@@ -9,22 +9,25 @@ use alloc::vec;
 use lib::{exit, thread_create, waittid};
 
 pub fn thread_a() -> ! {
-    for _ in 0..1000 {
-        print!("a");
+    for i in 26..=33 {
+        let r = fibonacci(i);
+        println!("A[{}] {}", i, r);
     }
     exit(1)
 }
 
 pub fn thread_b() -> ! {
-    for _ in 0..1000 {
-        print!("b");
+    for i in 26..=33 {
+        let r = fibonacci(i);
+        println!("B[{}] {}", i, r);
     }
     exit(2)
 }
 
 pub fn thread_c() -> ! {
-    for _ in 0..1000 {
-        print!("c");
+    for i in 26..=33 {
+        let r = fibonacci(i);
+        println!("C[{}] {}", i, r);
     }
     exit(3)
 }
@@ -38,8 +41,15 @@ pub fn main() -> i32 {
     ];
     for tid in v.iter() {
         let exit_code = waittid(*tid as usize);
-        println!("thread#{} exited with code {}", tid, exit_code);
+        println!("thread[{}] exit({})", tid, exit_code);
     }
     println!("main thread exited.");
     0
+}
+
+// 一个耗时程序，用以验证10ms之后会自动切换下一个任务
+pub fn fibonacci(x: u32) -> u32 {
+    if x == 0 { return 0 }
+    if x == 1 { return 1 }
+    fibonacci(x - 2) + fibonacci(x - 1)
 }
