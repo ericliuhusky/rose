0f0a13a9fdc5f7763a054f896ab0cea0506f1155
EricLiu <ericliuhusky@icloud.com>
2023-04-06 16:23:25 +0800

    :boom: 合并引导栈和内核栈，栈由在代码的.data段上分配改为在内存上分配

--- crates/entry/src/lib.rs
+++ crates/entry/src/lib.rs
@@ -6,12 +6,9 @@ use core::arch::asm;
 #[no_mangle]
 #[link_section = ".text.entry"]
 fn _start() {
-    let top = BOOT_STACK.as_ptr() as usize + BOOT_STACK.len();
     unsafe {
-        asm!("mv sp, {}", in(reg) top);
+        // TODO: KERNEL_STACK_TOP
+        asm!("li sp, 0x87800000");
         asm!("call main")
     }
 }
-
-#[link_section = ".data"]
-static BOOT_STACK: [u8; 0x10000] = [0; 0x10000];
