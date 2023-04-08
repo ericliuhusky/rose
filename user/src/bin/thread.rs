b63aba00d0976d5a0e6cdc55613ebf64103cf409
EricLiu <ericliuhusky@icloud.com>
2023-04-08 10:59:56 +0800

    :fire: fibonacci sleep来模拟耗时程序

--- user/src/bin/thread.rs
+++ user/src/bin/thread.rs
@@ -6,28 +6,28 @@ extern crate lib;
 extern crate alloc;
 
 use alloc::vec;
-use lib::{exit, thread_create, waittid};
+use lib::{exit, thread_create, waittid, sleep};
 
 pub fn thread_a() -> ! {
-    for i in 26..=33 {
-        let r = fibonacci(i);
-        println!("A[{}] {}", i, r);
+    for i in 0..3 {
+        sleep(1);
+        println!("A {}", i);
     }
     exit(1)
 }
 
 pub fn thread_b() -> ! {
-    for i in 26..=33 {
-        let r = fibonacci(i);
-        println!("B[{}] {}", i, r);
+    for i in 0..3 {
+        sleep(1);
+        println!("B {}", i);
     }
     exit(2)
 }
 
 pub fn thread_c() -> ! {
-    for i in 26..=33 {
-        let r = fibonacci(i);
-        println!("C[{}] {}", i, r);
+    for i in 0..3 {
+        sleep(1);
+        println!("C {}", i);
     }
     exit(3)
 }
@@ -45,11 +45,4 @@ pub fn main() -> i32 {
     }
     println!("main thread exited.");
     0
-}
-
-// 一个耗时程序，用以验证10ms之后会自动切换下一个任务
-pub fn fibonacci(x: u32) -> u32 {
-    if x == 0 { return 0 }
-    if x == 1 { return 1 }
-    fibonacci(x - 2) + fibonacci(x - 1)
 }
