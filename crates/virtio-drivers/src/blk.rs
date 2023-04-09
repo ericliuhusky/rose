3bc2b5fc3f2762869fcd7fa241e80b3cef3e245b
EricLiu <ericliuhusky@icloud.com>
2023-04-09 19:30:22 +0800

    :fire: BlkFeature

--- crates/virtio-drivers/src/blk.rs
+++ crates/virtio-drivers/src/blk.rs
@@ -20,11 +20,8 @@ impl<H: Hal> VirtIOBlk<'_, H> {
     /// Create a new VirtIO-Blk driver.
     pub fn new(header: &'static mut VirtIOHeader) -> Result<Self> {
         header.begin_init(|features| {
-            let features = BlkFeature::from_bits_truncate(features);
-            info!("device features: {:?}", features);
             // negotiate these flags only
-            let supported_features = BlkFeature::empty();
-            (features & supported_features).bits()
+            0
         });
 
         // read configuration space
@@ -258,55 +255,6 @@ impl Default for BlkResp {
 }
 
 const BLK_SIZE: usize = 512;
-
-bitflags! {
-    struct BlkFeature: u64 {
-        /// Device supports request barriers. (legacy)
-        const BARRIER       = 1 << 0;
-        /// Maximum size of any single segment is in `size_max`.
-        const SIZE_MAX      = 1 << 1;
-        /// Maximum number of segments in a request is in `seg_max`.
-        const SEG_MAX       = 1 << 2;
-        /// Disk-style geometry specified in geometry.
-        const GEOMETRY      = 1 << 4;
-        /// Device is read-only.
-        const RO            = 1 << 5;
-        /// Block size of disk is in `blk_size`.
-        const BLK_SIZE      = 1 << 6;
-        /// Device supports scsi packet commands. (legacy)
-        const SCSI          = 1 << 7;
-        /// Cache flush command support.
-        const FLUSH         = 1 << 9;
-        /// Device exports information on optimal I/O alignment.
-        const TOPOLOGY      = 1 << 10;
-        /// Device can toggle its cache between writeback and writethrough modes.
-        const CONFIG_WCE    = 1 << 11;
-        /// Device can support discard command, maximum discard sectors size in
-        /// `max_discard_sectors` and maximum discard segment number in
-        /// `max_discard_seg`.
-        const DISCARD       = 1 << 13;
-        /// Device can support write zeroes command, maximum write zeroes sectors
-        /// size in `max_write_zeroes_sectors` and maximum write zeroes segment
-        /// number in `max_write_zeroes_seg`.
-        const WRITE_ZEROES  = 1 << 14;
-
-        // device independent
-        const NOTIFY_ON_EMPTY       = 1 << 24; // legacy
-        const ANY_LAYOUT            = 1 << 27; // legacy
-        const RING_INDIRECT_DESC    = 1 << 28;
-        const RING_EVENT_IDX        = 1 << 29;
-        const UNUSED                = 1 << 30; // legacy
-        const VERSION_1             = 1 << 32; // detect legacy
-
-        // the following since virtio v1.1
-        const ACCESS_PLATFORM       = 1 << 33;
-        const RING_PACKED           = 1 << 34;
-        const IN_ORDER              = 1 << 35;
-        const ORDER_PLATFORM        = 1 << 36;
-        const SR_IOV                = 1 << 37;
-        const NOTIFICATION_DATA     = 1 << 38;
-    }
-}
 
 unsafe impl AsBuf for BlkReq {}
 unsafe impl AsBuf for BlkResp {}
