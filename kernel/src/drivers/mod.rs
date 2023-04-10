7fc47d1e6a5462e92878966af6cc597b6ab27b7c
EricLiu <ericliuhusky@icloud.com>
2023-04-10 14:51:51 +0800

    :sparkles: net driver

--- kernel/src/drivers/mod.rs
+++ kernel/src/drivers/mod.rs
@@ -1,4 +1,5 @@
 pub mod virtio_block;
 mod virtio_bus;
+pub mod virtio_net;
 
 pub use virtio_block::BLOCK_DEVICE;
---
+++ kernel/src/drivers/virtio_net.rs
@@ -0,0 +1,38 @@
+use super::virtio_bus::VirtioHal;
+use alloc::rc::Rc;
+use core::cell::RefCell;
+use lazy_static::*;
+use virtio_drivers::{VirtIOHeader, VirtIONet};
+
+const VIRTIO8: usize = 0x10007000;
+
+lazy_static! {
+    pub static ref NET_DEVICE: Rc<dyn NetDevice> = Rc::new(VirtIONetWrapper::new());
+}
+
+pub trait NetDevice {
+    fn transmit(&self, data: &[u8]);
+    fn receive(&self, data: &mut [u8]) -> usize;
+}
+
+pub struct VirtIONetWrapper(RefCell<VirtIONet<'static, VirtioHal>>);
+
+impl NetDevice for VirtIONetWrapper {
+    fn transmit(&self, data: &[u8]) {
+        self.0.borrow_mut().send(data).expect("can't send data")
+    }
+
+    fn receive(&self, data: &mut [u8]) -> usize {
+        self.0.borrow_mut().recv(data).expect("can't receive data")
+    }
+}
+
+impl VirtIONetWrapper {
+    pub fn new() -> Self {
+        unsafe {
+            let virtio = VirtIONet::<VirtioHal>::new(&mut *(VIRTIO8 as *mut VirtIOHeader))
+                .expect("can't create net device by virtio");
+            VirtIONetWrapper(RefCell::new(virtio))
+        }
+    }
+}
