8dc5cdf8bd9116a83a05dc4ef229cb7765f82fe9
EricLiu <ericliuhusky@icloud.com>
2023-04-10 20:57:23 +0800

    :truck: fs_test -> fs_pack

--- fs_pack/src/main.rs
+++ fs_pack/src/main.rs
@@ -16,28 +16,12 @@ pub const INODE_BITMAP_BLOCK_NUM: u32 = 1;
 pub const INODE_AREA_BLOCK_NUM: u32 = 64;
 pub const DATA_BITMAP_BLOCK_NUM: u32 = 16;
 pub const DATA_AREA_BLOCK_NUM: u32 = DATA_BITMAP_BLOCK_NUM * 4096;
-pub const TOTAL_BLOCK_NUM: u32 = 1 + INODE_BITMAP_BLOCK_NUM + INODE_AREA_BLOCK_NUM + DATA_BITMAP_BLOCK_NUM + DATA_AREA_BLOCK_NUM;
-
+pub const TOTAL_BLOCK_NUM: u32 =
+    1 + INODE_BITMAP_BLOCK_NUM + INODE_AREA_BLOCK_NUM + DATA_BITMAP_BLOCK_NUM + DATA_AREA_BLOCK_NUM;
 
 static mut BLOCKS: [[u8; 0x200]; TOTAL_BLOCK_NUM as usize] = [[0; 0x200]; TOTAL_BLOCK_NUM as usize];
 
 pub struct MemoryBlockDevice;
-
-impl MemoryBlockDevice {
-    fn show_block(i: usize) {
-        let blocks = unsafe { &BLOCKS };
-        let block = blocks[i];
-        println!("[{}]", i);
-        for j in 0..0x20 {
-            print!("{:03x}:  ", j * 0x10);
-            for k in 0..0x10 {
-                let byte = block[j * 0x10 + k];
-                print!("{:02x} ", byte);
-            }
-            println!();
-        }
-    }
-}
 
 impl BlockDevice for MemoryBlockDevice {
     fn read_block(&self, block_id: usize, buf: &mut [u8]) {
@@ -57,7 +41,13 @@ impl BlockDevice for MemoryBlockDevice {
 
 pub fn fs_pack() {
     let block_device = Rc::new(MemoryBlockDevice);
-    let mut fs = FileSystem::create(block_device, INODE_BITMAP_BLOCK_NUM, INODE_AREA_BLOCK_NUM, DATA_BITMAP_BLOCK_NUM, DATA_AREA_BLOCK_NUM);
+    let mut fs = FileSystem::create(
+        block_device,
+        INODE_BITMAP_BLOCK_NUM,
+        INODE_AREA_BLOCK_NUM,
+        DATA_BITMAP_BLOCK_NUM,
+        DATA_AREA_BLOCK_NUM,
+    );
 
     let apps: Vec<String> = read_dir("../user/src/bin")
         .unwrap()
@@ -85,4 +75,8 @@ pub fn fs_pack() {
     ))
     .unwrap();
     f.write_all(unsafe { &BLOCKS.concat() }).unwrap();
+}
+
+fn main() {
+    fs_pack();
 }
