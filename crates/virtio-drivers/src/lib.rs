0582151eba5d3dd7d9ed49b379d762369294cba0
EricLiu <ericliuhusky@icloud.com>
2023-04-11 17:49:47 +0800

    :fire: Result Error

--- crates/virtio-drivers/src/lib.rs
+++ crates/virtio-drivers/src/lib.rs
@@ -20,26 +20,6 @@ use hal::*;
 /// The page size in bytes supported by the library (4 KiB).
 const PAGE_SIZE: usize = 0x1000;
 
-/// The type returned by driver methods.
-pub type Result<T = ()> = core::result::Result<T, Error>;
-
-/// The error type of VirtIO drivers.
-#[derive(Copy, Clone, Debug, Eq, PartialEq)]
-pub enum Error {
-    /// The buffer is too small.
-    BufferTooSmall,
-    /// The device is not ready.
-    NotReady,
-    /// The queue is already in use.
-    AlreadyUsed,
-    /// Invalid parameter.
-    InvalidParam,
-    /// Failed to alloc DMA memory.
-    DmaError,
-    /// I/O Error
-    IoError,
-}
-
 /// Align `size` up to a page.
 fn align_up(size: usize) -> usize {
     (size + PAGE_SIZE) & !(PAGE_SIZE - 1)
