ab7b263c9d446e95d056d4099f2307ec898dca7e
EricLiu <ericliuhusky@icloud.com>
2023-04-15 12:27:34 +0800

    :fire: unused

--- kernel/src/net/mod.rs
+++ kernel/src/net/mod.rs
@@ -249,37 +249,3 @@ pub fn busy_wait_udp_read(lport: u16) -> (Vec<u8>, IPv4, u16) {
         }
     }
 }
-
-#[allow(unused)]
-pub fn hexdump(data: &[u8]) {
-    const PRELAND_WIDTH: usize = 70;
-    println!("[kernel] {:-^1$}", " hexdump ", PRELAND_WIDTH);
-    for offset in (0..data.len()).step_by(16) {
-        print!("[kernel] ");
-        for i in 0..16 {
-            if offset + i < data.len() {
-                print!("{:02x} ", data[offset + i]);
-            } else {
-                print!("{:02} ", "");
-            }
-        }
-
-        print!("{:>6}", ' ');
-
-        for i in 0..16 {
-            if offset + i < data.len() {
-                let c = data[offset + i];
-                if c >= 0x20 && c <= 0x7e {
-                    print!("{}", c as char);
-                } else {
-                    print!(".");
-                }
-            } else {
-                print!("{:02} ", "");
-            }
-        }
-
-        println!("");
-    }
-    println!("[kernel] {:-^1$}", " hexdump end ", PRELAND_WIDTH);
-}
