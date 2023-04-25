aa251f5b89e33e38a8d8463f1124468e842bcd58
EricLiu <ericliuhusky@icloud.com>
2023-04-25 12:33:41 +0800

    :art: transmute read

--- kernel/src/net/mod.rs
+++ kernel/src/net/mod.rs
@@ -2,7 +2,7 @@ pub mod port_table;
 pub mod tcp;
 pub mod udp;
 
-use core::mem::size_of;
+use core::mem::{size_of, transmute};
 use crate::{
     drivers::virtio_net::NET_DEVICE,
 };
@@ -137,7 +137,7 @@ pub enum EthType {
 }
 
 #[derive(Clone, Copy, Default)]
-#[repr(C)]
+#[repr(packed)]
 pub struct Eth {
     pub dhost: [u8; 6], // destination host
     pub shost: [u8; 6], // source host
@@ -291,43 +291,21 @@ impl TCPHeader {
 }
 
 
-struct Link;
-
-impl Link {
-    fn recv_eth() -> (Eth, Vec<u8>) {
-        let mut recv_buf = vec![0u8; 1024];
-        let len = NET_DEVICE.receive(&mut recv_buf);
-        let data = &recv_buf[..len];
-
-        let eth_len = size_of::<Eth>();
-        let remain_data = &data[eth_len..];
-        let eth = unsafe { &*(&data[..eth_len] as *const _ as *const Eth) };
-
-        (*eth, remain_data.to_vec())
-    }
-
-    fn send_eth(eth: Eth, data: Vec<u8>) {
-        let mut re_eth = eth;
-        re_eth.dhost = eth.shost;
-        re_eth.shost = LOCALHOST_MAC.to_bytes();
-
-        let eth_data = unsafe { &*(&re_eth as *const Eth as *const [u8; 14]) };
-        let eth_data = eth_data.to_vec();
-        let mut reply_data = eth_data;
-        reply_data.extend(data);
-
-        NET_DEVICE.transmit(&reply_data)
-    }
-}
-
 struct Net;
 
 impl Net {
     fn recv_arp() -> Option<(Eth, Arp)> {
-        let (eth, data) = Link::recv_eth();
+        let mut recv_buf = vec![0u8; 1024];
+        let len = NET_DEVICE.receive(&mut recv_buf);
+        let data = &recv_buf[..len];
+
+        let eth_ptr = data.as_ptr() as *const Eth;
+        let arp_ptr = unsafe { eth_ptr.offset(1) } as *const Arp;
+        let eth = unsafe { core::ptr::read(eth_ptr) };
+        let arp = unsafe { core::ptr::read(arp_ptr) };
+
         if eth.type_() == EthType::ARP {
-            let arp = unsafe { &*(&data[..] as *const [u8] as *const Arp) };
-            Some((eth, *arp))
+            Some((eth, arp))
         } else {
             None
         }
@@ -341,39 +319,17 @@ impl Net {
         re_arp.tha = arp.sha;
         re_arp.set_type(ArpType::Reply);
 
-        let data = unsafe { &*(&re_arp as *const Arp as *const [u8; 28]) };
-        let data = data.to_vec();
+        let mut re_eth = eth;
+        re_eth.dhost = eth.shost;
+        re_eth.shost = LOCALHOST_MAC.to_bytes();
 
-        Link::send_eth(eth, data);
-    }
+        let eth_data: [u8; size_of::<Eth>()] = unsafe { transmute(re_eth) };
+        let arp_data: [u8; size_of::<Arp>()] = unsafe { transmute(re_arp) };
+        let mut data = Vec::new();
+        data.extend(eth_data);
+        data.extend(arp_data);
 
-    fn recv_ip() -> Option<(Eth, Ip, Vec<u8>)> {
-        let (eth, data) = Link::recv_eth();
-        if eth.type_() == EthType::IP {
-            let ip_len = size_of::<Ip>();
-            let ip = unsafe { &*(&data[..ip_len] as *const [u8] as *const Ip) };
-            let remain_data = &data[ip_len..];
-            Some((eth, *ip, remain_data.to_vec()))
-        } else {
-            None
-        }
-    }
-
-    fn send_ip(eth: Eth, ip: Ip, data: Vec<u8>) {
-        let len = data.len() + size_of::<Ip>();
-        let mut re_ip = ip;
-        re_ip.src = ip.dst;
-        re_ip.dst = ip.src;
-        re_ip.len = (len as u16).to_be();
-        re_ip.sum = 0;
-        re_ip.sum = check_sum(&mut re_ip as *mut Ip as *mut u8, size_of::<Ip>() as _, 0);
-
-        let header_data = unsafe { &*(&re_ip as *const Ip as *const [u8; 20]) };
-        let header_data = header_data.to_vec();
-
-        let mut total_data = header_data;
-        total_data.extend(data);
-        Link::send_eth(eth, total_data)
+        NET_DEVICE.transmit(&data);
     }
 }
 
@@ -381,19 +337,21 @@ struct TransPort;
 
 impl TransPort {
     fn recv_udp(port: u16) -> Option<(Eth, Ip, UDPHeader, Vec<u8>)> {
-        if let Some((eth, ip, data)) = Net::recv_ip() {
-            if ip.protocol() == IPProtocal::UDP {
-                let udp_len = size_of::<UDPHeader>();
-                let udp = unsafe { &*(&data[..udp_len] as *const [u8] as *const UDPHeader) };
-                if udp.dport.to_be() == port {
-                    let remain_data = &data[udp_len..];
-                    Some((eth, ip, *udp, remain_data.to_vec()))
-                } else {
-                    None
-                }
-            } else {
-                None
-            }
+        let mut recv_buf = vec![0u8; 1024];
+        let len = NET_DEVICE.receive(&mut recv_buf);
+        let data = &recv_buf[..len];
+
+        let eth_ptr = data.as_ptr() as *const Eth;
+        let ip_ptr = unsafe { eth_ptr.offset(1) } as *const Ip;
+        let udp_ptr = unsafe { ip_ptr.offset(1) } as *const UDPHeader;
+        let data_ptr = unsafe { udp_ptr.offset(1) } as *const u8;
+        let eth = unsafe { core::ptr::read(eth_ptr) };
+        let ip = unsafe { core::ptr::read(ip_ptr) };
+        let udp = unsafe { core::ptr::read(udp_ptr) };
+        let data = unsafe { core::slice::from_raw_parts(data_ptr, len - size_of::<Eth>() - size_of::<Ip>() - size_of::<UDPHeader>()) };
+
+        if eth.type_() == EthType::IP && ip.protocol() == IPProtocal::UDP && udp.dport.to_be() == port {
+            Some((eth, ip, udp, data.to_vec()))
         } else {
             None
         }
@@ -407,30 +365,50 @@ impl TransPort {
         re_udp.sum = 0;
         re_udp.ulen = (len as u16).to_be();
 
-        let header_data = unsafe { &*(&re_udp as *const UDPHeader as *const [u8; 8]) };
-        let header_data = header_data.to_vec();
+        let len = data.len() + size_of::<Ip>() + size_of::<UDPHeader>();
+        let mut re_ip = ip;
+        re_ip.src = ip.dst;
+        re_ip.dst = ip.src;
+        re_ip.len = (len as u16).to_be();
+        re_ip.sum = 0;
+        re_ip.sum = check_sum(&mut re_ip as *mut Ip as *mut u8, size_of::<Ip>() as _, 0);
+
+        let mut re_eth = eth;
+        re_eth.dhost = eth.shost;
+        re_eth.shost = LOCALHOST_MAC.to_bytes();
+
+        let eth_data: [u8; size_of::<Eth>()] = unsafe { transmute(re_eth) };
+        let ip_data: [u8; size_of::<Ip>()] = unsafe { transmute(re_ip) };
+        let udp_data: [u8; size_of::<UDPHeader>()] = unsafe { transmute(re_udp) };
+        let mut header_data = Vec::new();
+        header_data.extend(eth_data);
+        header_data.extend(ip_data);
+        header_data.extend(udp_data);
+
         let mut total_data = header_data;
         total_data.extend(data);
 
-        Net::send_ip(eth, ip, total_data);
+        NET_DEVICE.transmit(&total_data);
     }
 
     fn recv_tcp(port: u16) -> Option<(Eth, Ip, TCPHeader, Vec<u8>)> {
-        if let Some((eth, ip, data)) = Net::recv_ip() {
-            if ip.protocol() == IPProtocal::TCP {
-                let tcp_len = size_of::<TCPHeader>();
-                let tcp = unsafe { &*(&data[..tcp_len] as *const [u8] as *const TCPHeader) };
-                if tcp.dport.to_be() == port {
-                    let remain_data = &data[tcp_len..];
-                    let offset = ((tcp.offset >> 4 & 0xf) as usize - 5) * 4;
-                    let offset_data = &remain_data[offset..];
-                    Some((eth, ip, *tcp, offset_data.to_vec()))
-                } else {
-                    None
-                }
-            } else {
-                None
-            }
+        let mut recv_buf = vec![0u8; 1024];
+        let len = NET_DEVICE.receive(&mut recv_buf);
+        let data = &recv_buf[..len];
+
+        let eth_ptr = data.as_ptr() as *const Eth;
+        let ip_ptr = unsafe { eth_ptr.offset(1) } as *const Ip;
+        let tcp_ptr = unsafe { ip_ptr.offset(1) } as *const TCPHeader;
+        let data_ptr = unsafe { tcp_ptr.offset(1) } as *const u8;
+        let eth = unsafe { core::ptr::read(eth_ptr) };
+        let ip = unsafe { core::ptr::read(ip_ptr) };
+        let tcp = unsafe { core::ptr::read(tcp_ptr) };
+        let data = unsafe { core::slice::from_raw_parts(data_ptr, len - size_of::<Eth>() - size_of::<Ip>() - size_of::<TCPHeader>()) };
+
+        if eth.type_() == EthType::IP && ip.protocol() == IPProtocal::TCP && tcp.dport.to_be() == port {
+            let offset = ((tcp.offset >> 4 & 0xf) as usize - 5) * 4;
+            let offset_data = &data[offset..];
+            Some((eth, ip, tcp, offset_data.to_vec()))
         } else {
             None
         }
@@ -448,40 +426,59 @@ impl TransPort {
         sum += (ip.pro as u16).to_be() as u32;
         sum += ((data.len() + size_of::<TCPHeader>()) as u16).to_be() as u32;
 
-        let mut check_sum = sum;
+        let mut ck_sum = sum;
         let start = &re_tcp as *const _ as usize;
         let end = start + size_of::<TCPHeader>();
         for p in (start..end).step_by(2) {
-            check_sum += unsafe { *(p as *const u16) as u32 };
-            if check_sum > 0xffff {
-                check_sum = (check_sum & 0xFFFF) + (check_sum >> 16);
-                check_sum = check_sum + (check_sum >> 16);
+            ck_sum += unsafe { *(p as *const u16) as u32 };
+            if ck_sum > 0xffff {
+                ck_sum = (ck_sum & 0xFFFF) + (ck_sum >> 16);
+                ck_sum = ck_sum + (ck_sum >> 16);
             }
         }
         let start = data.as_slice().as_ptr() as usize;
         let end = start + UInt(data.len()).align_to_lower(2);
         for p in (start..end).step_by(2) {
-            check_sum += unsafe { *(p as *const u16) as u32 };
-            if check_sum > 0xffff {
-                check_sum = (check_sum & 0xFFFF) + (check_sum >> 16);
-                check_sum = check_sum + (check_sum >> 16);
+            ck_sum += unsafe { *(p as *const u16) as u32 };
+            if ck_sum > 0xffff {
+                ck_sum = (ck_sum & 0xFFFF) + (ck_sum >> 16);
+                ck_sum = ck_sum + (ck_sum >> 16);
             }
         }
         if data.len() %2 != 0 {
-            check_sum += *data.last().unwrap() as u32;
-            check_sum = (check_sum & 0xFFFF) + (check_sum >> 16);
-            check_sum = check_sum + (check_sum >> 16);
+            ck_sum += *data.last().unwrap() as u32;
+            ck_sum = (ck_sum & 0xFFFF) + (ck_sum >> 16);
+            ck_sum = ck_sum + (ck_sum >> 16);
         }
-        let ans = !check_sum as u16;
+        let ans = !ck_sum as u16;
 
         re_tcp.sum = ans;
 
-        let header_data = unsafe { &*(&re_tcp as *const TCPHeader as *const [u8; 20]) };
-        let header_data = header_data.to_vec();
+
+        let len = data.len() + size_of::<Ip>() + size_of::<TCPHeader>();
+        let mut re_ip = ip;
+        re_ip.src = ip.dst;
+        re_ip.dst = ip.src;
+        re_ip.len = (len as u16).to_be();
+        re_ip.sum = 0;
+        re_ip.sum = check_sum(&mut re_ip as *mut Ip as *mut u8, size_of::<Ip>() as _, 0);
+
+        let mut re_eth = eth;
+        re_eth.dhost = eth.shost;
+        re_eth.shost = LOCALHOST_MAC.to_bytes();
+
+        let eth_data: [u8; size_of::<Eth>()] = unsafe { transmute(re_eth) };
+        let ip_data: [u8; size_of::<Ip>()] = unsafe { transmute(re_ip) };
+        let tcp_data: [u8; size_of::<TCPHeader>()] = unsafe { transmute(re_tcp) };
+        let mut header_data = Vec::new();
+        header_data.extend(eth_data);
+        header_data.extend(ip_data);
+        header_data.extend(tcp_data);
+
         let mut total_data = header_data;
         total_data.extend(data);
 
-        Net::send_ip(eth, ip, total_data);
+        NET_DEVICE.transmit(&total_data);
     }
 }
 
